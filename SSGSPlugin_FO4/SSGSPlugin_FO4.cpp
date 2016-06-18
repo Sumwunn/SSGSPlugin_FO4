79f085617c13aa4274b1896e5feb6f08b112cf09
Sumwunn <0x77VA@gmail.com>
2016-06-18 15:49:18 -0400

    Updated to use PlayerStatsAPI_FO4.

--- SSGSPlugin_FO4/SSGSPlugin_FO4.cpp
+++ SSGSPlugin_FO4/SSGSPlugin_FO4.cpp
@@ -47,25 +47,12 @@ using curl::curl_easy_exception;
 // Has to be after curl or terrible compile error.
 #include <windows.h>
 #include <ShlObj.h>
-
-// Fallout 4 version specific data.
-void* StatsPointerRetrieverRVA = (void*)NULL; // Address that will give us Health & Stamina values.
-void* StatsPointerRetrieverVA = (void*)NULL;
-void* CurrentCarryWeightRVA = (void*)NULL; // Address that will give us the players current weight value.
-extern "C" void* CurrentCarryWeightVA; // For StatsRetriver.asm.
-void* CurrentCarryWeightVA = (void*)NULL;
+#include "PlayerStatsAPI_FO4.h"
 
 // Defined functions.
-// ASM
-extern "C" int GetEntryPointRVA(HMODULE Module);
-extern "C" int HookWrite(void* HookAddress, void* HookDestination, DWORD HookRegister);
-extern "C" void* ConvertRVAToVA(HMODULE Module, void* Address);
-extern "C" int StatsPointerRetriever();
-extern "C" int StatsGetValue(DWORD StatsValue);
-extern "C" int CarryWeightGetCurrentValue();
 // C++
 DWORD WINAPI GameSenseDataSender(LPVOID lpParam);
-int PostJson(StringBuffer& Buffer, char* Address); // JSSSSSSOOOOOONNNNNNNNNNNN.
+void PostJson(StringBuffer& Buffer, char* Address); // JSSSSSSOOOOOONNNNNNNNNNNN.
 
 // Data we need across multiple functions here.
 #define corePropsJsonBufferSize MAX_PATH
@@ -116,38 +103,6 @@ int Setup() {
 
 	//////// Setup Part 2 - Addresses ////////
 
-	// Get module of target to writes hooks to.
-	HMODULE TargetModule = GetModuleHandle(NULL);
-
-	//// Fallout 4 Version Check ////
-	// Using the entry point rva from pe header as my verison check.
-	int Result = GetEntryPointRVA(TargetModule);
-	if (Result == 0x06BC3310) {
-		// Fill in version specific data (v1.5.307.0).
-		StatsPointerRetrieverRVA = (void*)0xE3DEE2;
-		CurrentCarryWeightRVA = (void*)0x5719050;
-		if (bConsoleLoggingEnabled) {
-			cout << "Fallout 4 v1.5.307.0 detected." << endl;
-		}
-	}
-#ifdef FO4_BETA_SUPPORT
-	else if (Result == 0x0) {
-		// Fill in version specific data (BETA).
-		StatsPointerRetrieverRVA = (void*)0x0;
-		CurrentCarryWeightRVA = (void*)0x0;
-		if (bConsoleLoggingEnabled) {
-			cout << "Fallout 4 v1.5.210.0 BETA detected." << endl;
-		}
-	}
-#endif
-	else {
-		// Unsupported version.
-		if (bConsoleLoggingEnabled) {
-			cout << "Fallout 4 unsupported version detected. Only v1.5.307.0.0 & v1.5.210.0 BETA supported." << endl;
-		}
-		return 0;
-	}
-
 	// Get path to SteelSeries Engine 3's coreProps.json and grab the address from it.
 	GetEnvironmentVariableA("PROGRAMDATA", SSGS_corePropsJsonPath, MAX_PATH);
 	strcat_s(SSGS_corePropsJsonPath, MAX_PATH, "\\SteelSeries\\SteelSeries Engine 3\\coreProps.json");
@@ -155,8 +110,12 @@ int Setup() {
 	FILE* fp;
 	if (fopen_s(&fp, SSGS_corePropsJsonPath, "rb") != 0) {
 		// No coreProps.json?!
+		if (bConsoleLoggingEnabled) {
+			cout << "coreProps.json not found. Make sure GameSense is enabled." << endl;
+		}
 		return 0;
 	}
+	// Parse json.
 	FileReadStream is(fp, SSGS_corePropsJsonBuffer, corePropsJsonBufferSize);
 	Document d;
 	d.ParseStream(is);
@@ -167,15 +126,21 @@ int Setup() {
 	strcpy_s(SSGS_ServerAddress_GameEvent, SSGS_ServerAddressBufferSize, SSGS_ServerAddress);
 	strcat_s(SSGS_ServerAddress_GameEvent, SSGS_ServerAddressBufferSize, "/game_event");
 
-	//////// Setup Part 3 - Hooks ////////
+	//////// Setup Part 3 - Hooks & Threads ////////
 
-	// Get the hook's addresses ready.
-	StatsPointerRetrieverVA = ConvertRVAToVA(TargetModule, StatsPointerRetrieverRVA);
-	CurrentCarryWeightVA = ConvertRVAToVA(TargetModule, CurrentCarryWeightRVA);
-
-	// Redirect an address from that game that will give us the values we need. (and hopefully consistently)
-	// Write Hook.
-	HookWrite(StatsPointerRetrieverVA, StatsPointerRetriever, 1);
+	int Result = InstallHook();
+	if (Result == -1) {
+		if (bConsoleLoggingEnabled) {
+			cout << "Incorrect process detected." << endl;
+			return 0;
+		}
+	}
+	else if (Result == -2) {
+		if (bConsoleLoggingEnabled) {
+			cout << "Unsupported version detected." << endl;
+			return 0;
+		}
+	}
 
 	// Setup threads.
 	GameSenseDataSender_hThread = CreateThread(NULL, NULL, GameSenseDataSender, NULL, NULL, NULL);
@@ -201,8 +166,6 @@ DWORD WINAPI GameSenseDataSender(LPVOID lpParam) {
 	StringBuffer Buffer;
 	// Current value.
 	int CurrentValue;
-	int CurrentValue2;
-	int CurrentValueDiff;
 
 	// Parse.
 	GameJsonParsed.Parse(GameDataJson);
@@ -218,40 +181,16 @@ DWORD WINAPI GameSenseDataSender(LPVOID lpParam) {
 
 		// In GameSense Interface: 0 = Dead, 100 = Good.
 
-		// Get current HEALTH value and send it to GameSense server.
-		CurrentValue = StatsGetValue(1);
+		// Get current Health value and send it to GameSense server.
+		CurrentValue = PlayerStatsGetValue(1);
 		// If current value is negative, set to ZERO.
-		if (CurrentValue < 0) {
-			Value& STR = GameJsonParsed["data"]["value"]; // Points to WEIGHT->Data->Value.
-			STR.SetInt(0); // Set current WEIGHT.
-			// Stringify the DOM.
-			Writer<StringBuffer> writer(Buffer);
-			GameJsonParsed.Accept(writer);
-			// Post.
-			PostJson(Buffer, SSGS_ServerAddress_GameEvent);
-		}
-		// If health is above 100, set to 100.
-		if (CurrentValue >= 100) {
-			// Modify it by DOM.
-			Value& STR = GameJsonParsed["data"]["value"]; // Points to Health->Data->Value.
-			STR.SetInt(100); // Set current health.
-			// Stringify the DOM.
-			Writer<StringBuffer> writer(Buffer);
-			GameJsonParsed.Accept(writer);
-			// Post.
-			PostJson(Buffer, SSGS_ServerAddress_GameEvent);
-		}
-		// Set HEALTH value.
-		else {
-			// Modify it by DOM.
-			Value& STR = GameJsonParsed["data"]["value"]; // Points to Health->Data->Value.
-			STR.SetInt(CurrentValue); // Set current health.
-			// Stringify the DOM.
-			Writer<StringBuffer> writer(Buffer);
-			GameJsonParsed.Accept(writer);
-			// Post.
-			PostJson(Buffer, SSGS_ServerAddress_GameEvent);
-		}
+		Value& STR = GameJsonParsed["data"]["value"]; // Points to HEALTH->Data->Value.
+		STR.SetInt(CurrentValue); // Set current Health.
+		// Stringify the DOM.
+		Writer<StringBuffer> writer(Buffer);
+		GameJsonParsed.Accept(writer);
+		// Post.
+		PostJson(Buffer, SSGS_ServerAddress_GameEvent);
 
 		// Cleanup.
 		Buffer.Clear();
@@ -260,41 +199,16 @@ DWORD WINAPI GameSenseDataSender(LPVOID lpParam) {
 
 		// In GameSense Interface: 0 = None, 100 = Full.
 
-		// Get current STAMINA value and send it to GameSense server.
-		CurrentValue = StatsGetValue(2);
-		// If current value is negative, set to ZERO.
-		if (CurrentValue < 0) {
-			Value& STR = GameJson2Parsed["data"]["value"]; // Points to WEIGHT->Data->Value.
-			STR.SetInt(0); // Set current WEIGHT.
-			// Stringify the DOM.
-			Writer<StringBuffer> writer(Buffer);
-			GameJson2Parsed.Accept(writer);
-			// Post.
-			PostJson(Buffer, SSGS_ServerAddress_GameEvent);
-		}
-		// If STAMINA is above 100, set to 100.
-		if (CurrentValue >= 100) {
-			// Modify it by DOM.
-			Value& STR = GameJson2Parsed["data"]["value"]; // Points to STAMINA->Data->Value.
-			STR.SetInt(100); // Set current STAMINA.
-			// Stringify the DOM.
-			Writer<StringBuffer> writer(Buffer);
-			GameJson2Parsed.Accept(writer);
-			// Post.
-			PostJson(Buffer, SSGS_ServerAddress_GameEvent);
-		}
-		// Set STAMINA value.
-		else {
-			// 2. Modify it by DOM.
-			Value& STR = GameJson2Parsed["data"]["value"]; // Points to STAMINA->Data->Value.
-			STR.SetInt(CurrentValue); // Set current STAMINA.
-			// Stringify the DOM.
-			Writer<StringBuffer> writer(Buffer);
-			GameJson2Parsed.Accept(writer);
-			// Post.
-			PostJson(Buffer, SSGS_ServerAddress_GameEvent);
-		}
-
+		// Get current Stamina value and send it to GameSense server.
+		CurrentValue = PlayerStatsGetValue(2);
+		// Get JSON ready.
+		Value& STR2 = GameJson2Parsed["data"]["value"]; // Points to STAMINA->Data->Value.
+		STR2.SetInt(CurrentValue); // Set current Stamina.
+		// Stringify the DOM.
+		Writer<StringBuffer> writer2(Buffer);
+		GameJson2Parsed.Accept(writer2);
+		// Post.
+		PostJson(Buffer, SSGS_ServerAddress_GameEvent);
 		// Cleanup.
 		Buffer.Clear();
 
@@ -303,101 +217,40 @@ DWORD WINAPI GameSenseDataSender(LPVOID lpParam) {
 		// In GameSense Interface: 0 = No RADS, 100 = Max RADS.
 
 		// Get current RADS value and send it to GameSense server.
-		CurrentValue = StatsGetValue(3);
-		// If current value is negative, set to ZERO.
-		if (CurrentValue < 0) {
-			Value& STR = GameJson3Parsed["data"]["value"]; // Points to WEIGHT->Data->Value.
-			STR.SetInt(0); // Set current WEIGHT.
-			// Stringify the DOM.
-			Writer<StringBuffer> writer(Buffer);
-			GameJson3Parsed.Accept(writer);
-			// Post.
-			PostJson(Buffer, SSGS_ServerAddress_GameEvent);
-		}
-		// If RADS is above 100, set to 100.
-		if (CurrentValue >= 100) {
-			CurrentValue = CurrentValue / 10; // Divide RADS value to fall between 0 - 100 range instead of 1000.
-			Value& STR = GameJson3Parsed["data"]["value"]; // Points to RADS->Data->Value.
-			STR.SetInt(CurrentValue); // Set current RADS.
-			// Stringify the DOM.
-			Writer<StringBuffer> writer(Buffer);
-			GameJson3Parsed.Accept(writer);
-			// Post.
-			PostJson(Buffer, SSGS_ServerAddress_GameEvent);
-		}
-		// Set RADS value.
-		else {
-			CurrentValue = CurrentValue / 10; // Divide RADS value between 0 - 100 to fall between 0 - 10 range instead of 100.
-			Value& STR = GameJson3Parsed["data"]["value"]; // Points to RADS->Data->Value.
-			STR.SetInt(CurrentValue); // Set current RADS.
-			// Stringify the DOM.
-			Writer<StringBuffer> writer(Buffer);
-			GameJson3Parsed.Accept(writer);
-			// Post.
-			PostJson(Buffer, SSGS_ServerAddress_GameEvent);
-		}
-
+		CurrentValue = PlayerStatsGetValue(3);
+		// Get JSON ready.
+		Value& STR3 = GameJson3Parsed["data"]["value"]; // Points to RADS->Data->Value.
+		STR3.SetInt(CurrentValue); // Set current RADS.
+		// Stringify the DOM.
+		Writer<StringBuffer> writer3(Buffer);
+		GameJson3Parsed.Accept(writer3);
+		// Post.
+		PostJson(Buffer, SSGS_ServerAddress_GameEvent);
 		// Cleanup.
 		Buffer.Clear();
 
 		//////////////// -WEIGHT- ////////////////
 
-		// Weight difference = CurrentWeight difference to MaxWeight. Not calculated when difference is over 100.
 		// In GameSense Interface: 0 = Over encumbered, 100+ = Plenty left.
 
 		// Get current WEIGHT difference and send it to GameSense server.
-		CurrentValue = CarryWeightGetCurrentValue();
-		CurrentValue2 = StatsGetValue(4); // Get max weight.
-		CurrentValueDiff = abs(CurrentValue - CurrentValue2);
-		// If current values are negative, set to ZERO.
-		if (CurrentValue < 0 || CurrentValue2 < 0) {
-			Value& STR = GameJson4Parsed["data"]["value"]; // Points to WEIGHT->Data->Value.
-			STR.SetInt(0); // Set current WEIGHT.
-			// Stringify the DOM.
-			Writer<StringBuffer> writer(Buffer);
-			GameJson4Parsed.Accept(writer);
-			// Post.
-			PostJson(Buffer, SSGS_ServerAddress_GameEvent);
-		}
-		// If current weight exceeds max weight, set to ZERO.
-		if (CurrentValue > CurrentValue2) {
-			Value& STR = GameJson4Parsed["data"]["value"]; // Points to WEIGHT->Data->Value.
-			STR.SetInt(0); // Set current WEIGHT.
-			// Stringify the DOM.
-			Writer<StringBuffer> writer(Buffer);
-			GameJson4Parsed.Accept(writer);
-			// Post.
-			PostJson(Buffer, SSGS_ServerAddress_GameEvent);
-		}
-		// If difference is over 100, set to 100. (lots of room left)
-		else if (CurrentValueDiff >= 100) {
-			Value& STR = GameJson4Parsed["data"]["value"]; // Points to WEIGHT->Data->Value.
-			STR.SetInt(100); // Set current WEIGHT.
-			// Stringify the DOM.
-			Writer<StringBuffer> writer(Buffer);
-			GameJson4Parsed.Accept(writer);
-			// Post.
-			PostJson(Buffer, SSGS_ServerAddress_GameEvent);
-		}
-		// Set current WEIGHT difference value.
-		else {
-			Value& STR = GameJson4Parsed["data"]["value"]; // Points to WEIGHT->Data->Value.
-			STR.SetInt(CurrentValueDiff); // Set current WEIGHT difference.
-			// Stringify the DOM.
-			Writer<StringBuffer> writer(Buffer);
-			GameJson4Parsed.Accept(writer);
-			// Post.
-			PostJson(Buffer, SSGS_ServerAddress_GameEvent);
-		}
-
+		CurrentValue = PlayerStatsGetValue(4);
+		// Get JSON ready.
+		Value& STR4 = GameJson4Parsed["data"]["value"]; // Points to WEIGHT->Data->Value.
+		STR4.SetInt(CurrentValue); // Set current WEIGHT.
+		// Stringify the DOM.
+		Writer<StringBuffer> writer4(Buffer);
+		GameJson4Parsed.Accept(writer4);
+		// Post.
+		PostJson(Buffer, SSGS_ServerAddress_GameEvent);
 		// Cleanup.
 		Buffer.Clear();
 	}
 
-	return NULL;
+	return 0;
 }
 
-int PostJson(StringBuffer& Buffer, char* Address) {
+void PostJson(StringBuffer& Buffer, char* Address) {
 
 	curl_easy easy;
 	curl_header header;
@@ -412,5 +265,5 @@ int PostJson(StringBuffer& Buffer, char* Address) {
 		error.print_traceback();
 	}
 
-	return 0;
+	return;
 }
